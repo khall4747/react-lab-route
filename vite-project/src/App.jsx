30e131daabe57ac1308d7da0da1d5a0f41920032
kholudAlabdullah <khl444ud@gmail.com>
2023-06-08 15:28:49 +0300

    day5

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,23 +7,52 @@ import NotFound from './componet/notFound'
 import { Link } from 'react-router-dom'; //to add links in page
 import Book from './componet/Book';
 import Allbooks from './componet/allbooks'
+import { Button } from '@chakra-ui/react';
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [todo,setTodo]=useState("")
+  const [list,setlist]=useState([])
+  const list1 = ()=>{
+    setlist([...list,todo])
+  }
   return (
     <>
-  
-  <Link to="/book" >books</Link>
+   <input value={todo} onChange={(e)=>setTodo(e.target.value)}/>
+     <button onClick={list1}>submit</button>
+     
+      <ul>{list.map((item,key)=>(
+        <li key={item}>{item}</li>
+         
+
+      ))}</ul>
     
-    <Routes>
-      <Route path="/" element={<h1>hello</h1>} ></Route>
-      <Route path="/book" element={<Allbooks/>} />
-      <Route path="/book/:id" element={<Book/>} />
-      <Route path="/*" element={<NotFound/>}/>
-    
-      </Routes> 
     </>
+   
   )
 }
 
+/*  const [count, setName] = useState(0)
+  const a =2
+  const b=3
+<button onClick={ ()=>{setName(a*b)}} >button</button>
+  <button  onClick={()=>{setName(a-b)}}>button</button>
+  <button>button</button>
+   <p>{count}</p>*/
+//-----------------------------------------------------------
+   /* const [rate,setRate]=useState(0)
+     const [Text,setText]=useState("") sholud have inatial vale
+ const valueAl=(e)=>{
+  if(rate<=5  && Text=="" ){
+   alert("hello")
+  }
+ 
+ }
+  return (
+    <>
+   <h1>rate</h1> <br/>
+   <input type='range' max={10} min={0} value={rate} onChange={(e)=>{setRate(e.target.value)}}/><br/>
+   <textarea value={Text} onChange={(e)=>{setText(e.target.value)}}></textarea><br/>
+    <button onClick={valueAl}>submit</button>
+    </>
+   
+  )*/
 export default App
